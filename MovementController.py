1a687dd406ac73a44d3f72751eb61cb99e25c8b0
Mihajlo Mitrovic <52574997+m-mitrovic@users.noreply.github.com>
2019-10-02 13:59:45 -0400

    Added debug events

--- MovementController.py
+++ MovementController.py
@@ -1,3 +1,5 @@
+import pygame
+
 from Resources.RMatrix import RMatrix
 from Resources.Vector3 import Vector3
 
@@ -15,3 +17,19 @@ class MovementController():
         self.rotation = RMatrix()
 
         return(vector, rotation)
+
+    def key_down(self, event):
+        if event[pygame.K_SPACE]:
+            print("space")
+
+        if event[pygame.K_w]:
+            print("up")
+        
+        if event[pygame.K_a]:
+            print("left")
+
+        if event[pygame.K_s]:
+            print("down")
+        
+        if event[pygame.K_d]:
+            print("right")
