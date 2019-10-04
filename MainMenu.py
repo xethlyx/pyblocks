86e2c406716a5bc198e2fc0b745eba32301df4ea
Mihajlo Mitrovic <52574997+m-mitrovic@users.noreply.github.com>
2019-10-04 14:33:35 -0400

    Main menu update 1

--- MainMenu.py
+++ MainMenu.py
@@ -7,14 +7,19 @@ class MainMenu():
         pass
         self.registry = registry
 
+
     def render(self):
-        red=255
-        blue=0
-        green=0
-        left=50
-        top=50
-        width=90
-        height=90
+        
+        back = pygame.image.load('test.png')
+        self.registry.currentWindow.blit(back, (0, 0)) 
+
+        red = 255
+        blue = 0
+        green = 0
+        left = 50
+        top = 50
+        width = 90
+        height = 90
 
         pygame.draw.rect(self.registry.currentWindow, [red, blue, green], [left, top, width, height])
 
