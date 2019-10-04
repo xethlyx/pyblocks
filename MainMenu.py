d3c3a9405a949821e32604c3c2821fc0a25a2348
Mihajlo Mitrovic <52574997+m-mitrovic@users.noreply.github.com>
2019-10-04 14:08:29 -0400

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -1,10 +1,21 @@
 import pygame
 
+RED = (255, 0, 0)
+
 class MainMenu():
     def __init__(self, registry):
         pass
         self.registry = registry
-        button = pygame.Rect(100, 100, 50, 50)
+
+        red=255
+        blue=0
+        green=0
+        left=50
+        top=50
+        width=90
+        height=90
+
+        pygame.draw.rect(window, [red, blue, green], [left, top, width, height])
 
     def render(self):
         pass
