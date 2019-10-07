433721e51742f5c9d79975ec13a5883dcea7fa38
Mihajlo Mitrovic <52574997+m-mitrovic@users.noreply.github.com>
2019-10-07 14:36:08 -0400

    Main menu buttons

--- MainMenu.py
+++ MainMenu.py
@@ -16,9 +16,13 @@ class MainMenu():
         self.registry.currentWindow.blit(back, (0, 0))
         left = 50
         top = 655
-        width = 360
+        width = 380
         height = 100
         pygame.draw.rect(self.registry.currentWindow, [225, 112, 85], [left, top, width, height])
+        self.font = pygame.font.Font('upheavtt.ttf', 45)
+        self.font2 = pygame.font.Font('upheavtt.ttf', 120)
+        self.registry.currentWindow.blit(self.font.render('Single Player', True, (255,255,255)), (68, 680))
+        self.registry.currentWindow.blit(self.font2.render('PyBlocks', True, (255,255,255)), (60, 30))
 
     def render_first(self):
         pass
